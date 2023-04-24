3879ed9ee4407dfe4e832926f905f41d0c34fac5
Luis Caceres <lccaceres@outlook.com>
2023-04-23 21:06:10 -0600

    Add connection bd

---
+++ Sales.API/Data/SalesDataContex.cs
@@ -0,0 +1,18 @@
+﻿using Sales.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sales.API.Data
+{
+    public class SalesDataContex : DbContext
+    {
+        public SalesDataContex(DbContextOptions<SalesDataContex> options) : base(options) { }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
